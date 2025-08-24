a0891872966e169c6ab145dfe4ee539d4d2b4685
adolfitou <bysucsp44@gmail.com>
2025-08-24 15:48:11 +0200

    1.0.0d

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://adolfitou.github.io',
+    base: '/Portfolio/',        // <- nombre del repo con barra inicial y final
+    output: 'static',
+});
